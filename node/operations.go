b2a9d33a330ddd0dec2f8cba31270f63f8d4fd4f
jrjaro18 <rohanjaiswal1500@gmail.com>
2026-07-23 22:10:36 +0530

    add giving vote logic

--- node/operations.go
+++ node/operations.go
@@ -28,28 +28,28 @@ func (n *Node) askForVote() {
 	totalVotes.Store(1)
 
 	wg := sync.WaitGroup{}
-	
+
 	for _, peerID := range n.transport.GetPeers() {
 		wg.Add(1)
 		go func(id int) {
 			defer wg.Done()
 			args := RequestVoteArgs{
-				currentTerm: n.currentTerm,
-				candidateID: n.id,
-				lastLogTerm: n.persistentStore.LastTerm(),
+				currentTerm:  n.currentTerm,
+				candidateID:  n.id,
+				lastLogTerm:  n.persistentStore.LastTerm(),
 				lastLogIndex: n.persistentStore.LastIndex(),
 			}
 			reply, err := n.transport.SendRequestVote(id, args)
 			if err != nil {
 				log.Printf("Error %v", err.Error())
 			}
-			
+
 			n.mutex.Lock()
 			if reply.term > n.currentTerm {
 				log.Printf("Peer %v term: %v, candidate %v currentTerm: %v", id, reply.term, n.id, n.currentTerm)
 				n.currentTerm = reply.term
 				n.role = Follower
-				n.votedFor	 = -1
+				n.votedFor = -1
 				n.persistentStore.SaveTermAndVote(n.currentTerm, n.votedFor)
 			}
 			n.mutex.Unlock()
@@ -63,17 +63,44 @@ func (n *Node) askForVote() {
 	n.mutex.Lock()
 	defer n.mutex.Unlock()
 	if n.role != Candidate || n.currentTerm != electionTerm {
-    	return
+		return
 	}
 	if totalVotes.Load() > int32(len(n.transport.GetPeers())/2) {
 		log.Printf("Node %d received majority votes, becoming leader", n.id)
 		n.setRole(Leader)
-		n.currentTerm+=1
+		n.currentTerm += 1
 	}
 }
 
-func (n *Node) giveVote() {
-	// Implementation for giving a vote to a candidate node
+func (n *Node) giveVote(args RequestVoteArgs) RequestVoteReply {
+    n.mutex.Lock()
+    defer n.mutex.Unlock()
+
+    if args.currentTerm < n.currentTerm {
+        return RequestVoteReply{voteGranted: false, term: n.currentTerm}
+    }
+
+    if args.currentTerm > n.currentTerm {
+        n.currentTerm = args.currentTerm
+        n.votedFor = -1
+        n.role = Follower
+    }
+
+    lastTerm := n.persistentStore.LastTerm()
+    lastIndex := n.persistentStore.LastIndex()
+    logOK := args.lastLogTerm > lastTerm ||
+        (args.lastLogTerm == lastTerm && args.lastLogIndex >= lastIndex)
+
+    canVote := n.votedFor == -1 || n.votedFor == args.candidateID
+
+    if !canVote || !logOK {
+        n.persistentStore.SaveTermAndVote(n.currentTerm, n.votedFor) // term may have changed above even if vote denied
+        return RequestVoteReply{voteGranted: false, term: n.currentTerm}
+    }
+
+    n.votedFor = args.candidateID
+    n.persistentStore.SaveTermAndVote(n.currentTerm, n.votedFor)
+    return RequestVoteReply{voteGranted: true, term: n.currentTerm}
 }
 
 func (n *Node) receiveWriteRequest() {
